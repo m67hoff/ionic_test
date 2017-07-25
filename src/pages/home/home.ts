7bd8fc49b1571c8baf07799911a50b72f06150dc
m67hoff <mhoff@web.de>
2017-07-25 11:30:26 +0200

    async_await

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,45 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Storage } from '@ionic/storage';
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
+ 
+    apple: any;
+    banana: any;
+ 
+    constructor(public navCtrl: NavController, public storage: Storage) {
+ 
+    }
+ 
+    ionViewDidLoad(){
+ 
+        this.getFromStorageStandard().then((result) => {
+            this.apple = result;
+        });
+        
+        this.banana = this.getFromStorageAsync();
+ 
+    }
+ 
+    setInStorage(){
+        this.storage.set('apple', '3.99');
+        this.storage.set('banana', '4.50');
+    }
+ 
+    getFromStorageStandard(){
+ 
+        return this.storage.get('apple');
+ 
+    }
+ 
+    async getFromStorageAsync(){
+ 
+        return await this.storage.get('banana');
+ 
+    }
+ 
 }
