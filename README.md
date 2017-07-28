41e51b273efe1a47ec3d4f6cbac4516dfa455c85
m67hoff <mhoff@web.de>
2017-07-28 16:57:45 +0200

    geolocation

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ### Getting Started with Google Maps in Ionic 2
-
 https://www.joshmorony.com/getting-started-with-google-maps-in-ionic-2/
+
+#### Ionic 2 & 3: How to Use Google Maps & Geolocation
+https://www.joshmorony.com/ionic-2-how-to-use-google-maps-geolocation-video-tutorial/
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,33 +1,47 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Geolocation } from '@ionic-native/geolocation';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-   
+
   @ViewChild('map') mapElement
   map: any
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public geolocation: Geolocation
+  ) {
 
   }
 
-  ionViewDidLoad(){
+  ionViewDidLoad() {
     this.initMap()
-  } 
+  }
 
-  initMap(){
-      const latLng = new google.maps.LatLng( 50, 10 )
-      
-      const mapOptions: google.maps.MapOptions = {
-        center: latLng,
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.ROADMAP 
+  initMap() {
+
+    this.geolocation.getCurrentPosition().then(
+      (position) => {
+        let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+        let mapOptions = {
+          center: latLng,
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+
+        this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+      }, (err) => {
+        console.log(err);
       }
+    )
 
-      this.map = new google.maps.Map(this.mapElement.nativeElement,mapOptions)
   }
 
 }
