90f26617dec06b6425c05719519d6bd7b703ef72
m67hoff <mhoff@web.de>
2017-07-28 15:50:00 +0200

    part1 basic display

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -6,9 +6,28 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+   
+  @ViewChild('map') mapElement
+  map: any
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad(){
+    this.initMap()
+  } 
+
+  initMap(){
+      const latLng = new google.maps.LatLng( 50, 10 )
+      
+      const mapOptions: google.maps.MapOptions = {
+        center: latLng,
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP 
+      }
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement,mapOptions)
   }
 
 }
